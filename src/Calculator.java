db99fab5aab34417d6d0f3b8bd1607c0a7b88c96
NadiraM <atina0814@gmail.com>
2020-08-28 01:01:43 -0700

    Add subtract method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,9 @@
 public class Calculator {
     public void double add(double num1, double num2) {
         return num1 + num2;
+    }
 
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
     }
 }
