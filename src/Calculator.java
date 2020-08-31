08aed260386addce004a7fe074ac54c3671e7599
NadiraM <atina0814@gmail.com>
2020-08-30 23:32:28 -0700

    fixed conflict

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,18 +8,10 @@ public class Calculator {
         double result = num1 - num2;
         return result;
     }
-<<<<<<< HEAD
 
 
     public static double multiply(double num1, double num2) {
         return num1 * num2;
     }
-    
 
-=======
-       
-    public static double multiply(double num1, double num2) {
-        return num1 * num2;
-    }
->>>>>>> a5d4353b118185e423dec2aa8ac9ea0592d0e58a
 }
